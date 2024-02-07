b069c9edbbfef2a9c5e4215ef9dd6e1e4b9396e4
AK3847 <abdulkadir.co21d2@scet.ac.in>
2024-02-07 22:21:11 +0530

    fix

--- Mark_Attendance.py
+++ Mark_Attendance.py
@@ -18,14 +18,13 @@ encode_list, classnames=load_encodings(encodings_file)
 
 #user defined function to access the Student Attendance Excel sheet
 def write_to_excel(student_name):
-    wb=openpyxl.load_workbook('C:/Users/Taiyab/Desktop/AI project/AI/Student Attendance.xlsx')
+    wb=openpyxl.load_workbook('Student Attendance.xlsx')
     #wb=openpyxl.load_workbook('Student Attendance.xlsx')
     sheet=wb.active
     current_date_time=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     row=[student_name, current_date_time]
     sheet.append(row)
-    wb.save('C:/Users/Taiyab/Desktop/AI project/AI/Student Attendance.xlsx')
-    #wb.save('Student Attendance.xlsx')
+    wb.save('Student Attendance.xlsx')
     wb.close()
 
 print("Starting the camera to capture your pic...")
@@ -42,13 +41,13 @@ while True:
         cv2.rectangle(frame, (left, top), (right, bottom), (217,136, 31), 2)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord(' '):# the capturing stops when user presses spacebar
-        cv2.imwrite('C:/Users/Taiyab/Desktop/AI project/AI/captured_image.jpg', frame) #storing the captured frame as a jpg
+        cv2.imwrite('captured_image.jpg', frame) #storing the captured frame as a jpg
         break
 
 cap.release()
 cv2.destroyAllWindows()
 
-captured_image = cv2.imread('C:/Users/Taiyab/Desktop/AI project/AI/captured_image.jpg') 
+captured_image = cv2.imread('captured_image.jpg') 
 captured_image_rgb = cv2.cvtColor(captured_image, cv2.COLOR_BGR2RGB)
 captured_image_encoding = face_recognition.face_encodings(captured_image_rgb)
 
